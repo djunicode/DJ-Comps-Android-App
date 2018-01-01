dfe902fb099a0f65653f7913ac223f99d4f1476e
JaiKotia <32239054+JaiKotia@users.noreply.github.com>
2018-01-01 21:25:59 +0530

    Updated User.java

--- Entities/User.java
+++ Entities/User.java
@@ -13,32 +13,22 @@ import static android.arch.persistence.room.ForeignKey.CASCADE;
  */
 
 @Entity(foreignKeys = @ForeignKey(entity = Group.class,
-        parentColumns = {"group_id", "student", "year", "division"},
-        childColumns = {"group_id", "student", "year", "division"},
+        parentColumns = {"group_id"},
+        childColumns = {"group_id"},
         onDelete = CASCADE))
 
 
 public class User {
     @PrimaryKey public BigInteger sapid;
-    public char password;
     public char bio;
     public char name;
-    public float diskUtilization;
     public BigInteger group_id;
-    public boolean student;
-    public int year;
-    public char division;
 
-    public User(BigInteger sapid, char password, char bio, char name, float diskUtilization, BigInteger group_id, boolean student, int year, char division) {
+    public User(BigInteger sapid, char bio, char name,BigInteger group_id) {
         this.sapid = sapid;
-        this.password=password;
         this.bio=bio;
         this.name=name;
-        this.diskUtilization=diskUtilization;
         this.group_id=group_id;
-        this.student=student;
-        this.year=year;
-        this.division=division;
     }
 
 }
