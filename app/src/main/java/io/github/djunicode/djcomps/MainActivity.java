2c1f9351a190daa7e3d0c5f083c918358a135c0a
Jai Kotia <32239054+JaiKotia@users.noreply.github.com>
2018-03-01 09:55:35 +0530

    API Call Methods (#17)
    API Calls by @JaiKotia

--- app/src/main/java/io/github/djunicode/djcomps/MainActivity.java
+++ app/src/main/java/io/github/djunicode/djcomps/MainActivity.java
@@ -1,16 +1,16 @@
 package io.github.djunicode.djcomps;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import java.util.*;
-import android.view.View;
-
+import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText sapID = findViewById(R.id.sap_id);
+    EditText password = findViewById(R.id.password);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +19,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void login(View view)
-    {
+    public void login(View view) {
         Intent myIntent = new Intent(view.getContext(), Home.class);
         startActivityForResult(myIntent, 0);
+        HTTPRequests.onLoginRequest(sapID.getText().toString(), password.getText().toString());
     }
 
 }
+
+
+
+
